31cde4c2dab0c747762c1fe1c5b2c087d9c9f678
Juan Fran <juanfran.rubiales@gmail.com>
2017-06-04 18:21:53 +0200

    Añadiendo nuevas reglas de traducción

--- src/main/java/org/apertium/format/transpiler/transfer/MorphTrans2XML.java
+++ src/main/java/org/apertium/format/transpiler/transfer/MorphTrans2XML.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.antlr.v4.runtime.*;
-//import org.apertium.format.transpiler.transfer.TransferLexer;
-//import org.apertium.format.transpiler.transfer.TransferParser;
 
 /**
  *
@@ -19,23 +17,23 @@ public class MorphTrans2XML {
     
     public static void main(String[] args) {
                 
-//        if (args.length == 0) {
-//            help();
-//        } else {
-//            String filePath = args[0];
-//            System.out.println("File: " + filePath);
-//            System.out.println("Parsing...");
-//            try {
-//                ANTLRFileStream in = new ANTLRFileStream(filePath);
-//                TransferLexer lexer = new TransferLexer(in);
-//                CommonTokenStream tokens = new CommonTokenStream(lexer);
-//                TransferParser parser = new TransferParser(tokens);
-//                parser.stat();
-//            } catch (IOException ex) {
-//                Logger.getLogger(MorphTrans2XML.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//            System.out.println("Finished.");
-//        }
+        if (args.length == 0) {
+            help();
+        } else {
+            String filePath = args[0];
+            System.out.println("File: " + filePath);
+            System.out.println("Parsing...");
+            try {
+                ANTLRFileStream in = new ANTLRFileStream(filePath);
+                TransferLexer lexer = new TransferLexer(in);
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                TransferParser parser = new TransferParser(tokens);
+                parser.stat();
+            } catch (IOException ex) {
+                Logger.getLogger(MorphTrans2XML.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("Finished.");
+        }
     }
 
 }
